8ca224830aebe531e76e95d785875c1e25f83411
iComputiCs <139661473+coderPlusDesigner@users.noreply.github.com>
2024-04-12 03:51:49 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -34,10 +34,19 @@ watch(
         <component :is="Component" />
       </transition>
     </router-view>
+    <div class="mobile-tablet">
+      <h3>
+        Вы видите это сообщение, потому что, возможно, открыли его на своем мобильном устройстве или
+        планшете. Этот проект доступен на настольных компьютерах и ноутбуках, поэтому используйте их
+        для ознакомления с проектом.
+      </h3>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@import '../src/assets/breakpoints';
+
 #container {
   color: var(--text-color);
   transition: color 0.25s ease;
@@ -45,5 +54,26 @@ watch(
   background-color: var(--bg-color);
   width: 100%;
   min-height: 100vh;
+  .mobile-tablet {
+    position: fixed;
+    z-index: 9999;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: none;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--bg-color);
+    padding: 100px;
+    h3 {
+      font-size: 24px;
+      color: var(--text-color);
+      text-align: justify;
+    }
+    @include media('<=tablet') {
+      display: flex;
+    }
+  }
 }
 </style>
